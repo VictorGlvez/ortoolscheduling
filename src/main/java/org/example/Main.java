9bf1296bca34196847462c7d5d0288fc3b114f98
Victor <elsodah12@gmail.com>
2024-06-06 12:59:22 +0200

    asd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -38,86 +38,27 @@
 //}
 
 
-
-
-
-
-
-
-
-
 package org.example;
 
 import com.google.ortools.Loader;
-import com.google.ortools.sat.CpModel;
-import com.google.ortools.sat.CpSolver;
-import com.google.ortools.sat.CpSolverStatus;
-import com.google.ortools.sat.LinearExpr;
-import com.google.ortools.sat.LinearExprBuilder;
-import com.google.ortools.sat.Literal;
+import com.google.ortools.sat.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
-/** Nurses problem with schedule requests. */
 public class Main {
     public static void main(String[] args) {
         Loader.loadNativeLibraries();
-        final int numNurses = 5;
-        final int numDays = 7;
+
+        final int numNurses = 10;
+        final int numDays = 360;
         final int numShifts = 3;
+        final int vacationDays = 30;  // Número de días de vacaciones seguidos
 
         final int[] allNurses = IntStream.range(0, numNurses).toArray();
         final int[] allDays = IntStream.range(0, numDays).toArray();
         final int[] allShifts = IntStream.range(0, numShifts).toArray();
-
-        final int[][][] shiftRequests = new int[][][]{
-                {
-                        {0, 0, 1},
-                        {0, 0, 0},
-                        {0, 0, 0},
-                        {0, 0, 0},
-                        {0, 0, 1},
-                        {0, 1, 0},
-                        {0, 0, 1},
-                },
-                {
-                        {0, 0, 0},
-                        {0, 0, 0},
-                        {0, 1, 0},
-                        {0, 1, 0},
-                        {1, 0, 0},
-                        {0, 0, 0},
-                        {0, 0, 1},
-                },
-                {
-                        {0, 1, 0},
-                        {0, 1, 0},
-                        {0, 0, 0},
-                        {1, 0, 0},
-                        {0, 0, 0},
-                        {0, 1, 0},
-                        {0, 0, 0},
-                },
-                {
-                        {0, 0, 1},
-                        {0, 0, 0},
-                        {1, 0, 0},
-                        {0, 1, 0},
-                        {0, 0, 0},
-                        {1, 0, 0},
-                        {0, 0, 0},
-                },
-                {
-                        {0, 0, 0},
-                        {0, 0, 1},
-                        {0, 1, 0},
-                        {0, 0, 0},
-                        {1, 0, 0},
-                        {0, 1, 0},
-                        {0, 0, 0},
-                },
-        };
 
         // Creates the model.
         CpModel model = new CpModel();
@@ -167,50 +108,89 @@ public class Main {
             maxShiftsPerNurse = minShiftsPerNurse + 1;
         }
         for (int n : allNurses) {
-            LinearExprBuilder numShiftsWorked = LinearExpr.newBuilder();
+            LinearExprBuilder shiftsWorked = LinearExpr.newBuilder();
             for (int d : allDays) {
                 for (int s : allShifts) {
-                    numShiftsWorked.add(shifts[n][d][s]);
+                    shiftsWorked.add(shifts[n][d][s]);
                 }
             }
-            model.addLinearConstraint(numShiftsWorked, minShiftsPerNurse, maxShiftsPerNurse);
+            model.addLinearConstraint(shiftsWorked, minShiftsPerNurse, maxShiftsPerNurse);
         }
 
-        LinearExprBuilder obj = LinearExpr.newBuilder();
+        // Adding vacation constraints for each nurse
         for (int n : allNurses) {
-            for (int d : allDays) {
+            // Randomly assign a start day for the vacation
+            int vacationStart = (int) (Math.random() * (numDays - vacationDays));
+            for (int d = vacationStart; d < vacationStart + vacationDays; d++) {
                 for (int s : allShifts) {
-                    obj.addTerm(shifts[n][d][s], shiftRequests[n][d][s]);
+                    model.addEquality(shifts[n][d][s], 0);
                 }
             }
         }
-        model.maximize(obj);
 
-        // Creates a solver and solves the model.
         CpSolver solver = new CpSolver();
-        CpSolverStatus status = solver.solve(model);
+        solver.getParameters().setLinearizationLevel(0);
+        // Tell the solver to enumerate all solutions.
+        solver.getParameters().setEnumerateAllSolutions(true);
 
-        if (status == CpSolverStatus.OPTIMAL || status == CpSolverStatus.FEASIBLE) {
-            System.out.printf("Solution:%n");
-            for (int d : allDays) {
-                System.out.printf("Day %d%n", d);
-                for (int n : allNurses) {
-                    for (int s : allShifts) {
-                        if (solver.booleanValue(shifts[n][d][s])) {
-                            if (shiftRequests[n][d][s] == 1) {
-                                System.out.printf("  Nurse %d works shift %d (requested).%n", n, s);
-                            } else {
-                                System.out.printf("  Nurse %d works shift %d (not requested).%n", n, s);
+        // Display the first five solutions.
+        final int solutionLimit = 5;
+        class VarArraySolutionPrinterWithLimit extends CpSolverSolutionCallback {
+            public VarArraySolutionPrinterWithLimit(
+                    int[] allNurses, int[] allDays, int[] allShifts, Literal[][][] shifts, int limit) {
+                solutionCount = 0;
+                this.allNurses = allNurses;
+                this.allDays = allDays;
+                this.allShifts = allShifts;
+                this.shifts = shifts;
+                solutionLimit = limit;
+            }
+
+            @Override
+            public void onSolutionCallback() {
+                System.out.printf("Solution #%d:%n", solutionCount);
+                for (int d : allDays) {
+                    System.out.printf("Day %d%n", d);
+                    for (int n : allNurses) {
+                        boolean isWorking = false;
+                        for (int s : allShifts) {
+                            if (booleanValue(shifts[n][d][s])) {
+                                isWorking = true;
+                                System.out.printf("  Nurse %d work shift %d%n", n, s);
                             }
+                        }
+                        if (!isWorking) {
+                            System.out.printf("  Nurse %d does not work%n", n);
                         }
                     }
                 }
+                solutionCount++;
+                if (solutionCount >= solutionLimit) {
+                    System.out.printf("Stop search after %d solutions%n", solutionLimit);
+                    stopSearch();
+                }
             }
-            System.out.printf("Number of shift requests met = %f (out of %d)%n", solver.objectiveValue(),
-                    numNurses * minShiftsPerNurse);
-        } else {
-            System.out.printf("No optimal solution found !");
+
+            public int getSolutionCount() {
+                return solutionCount;
+            }
+
+            private int solutionCount;
+            private final int[] allNurses;
+            private final int[] allDays;
+            private final int[] allShifts;
+            private final Literal[][][] shifts;
+            private final int solutionLimit;
         }
+
+        VarArraySolutionPrinterWithLimit cb =
+                new VarArraySolutionPrinterWithLimit(allNurses, allDays, allShifts, shifts, solutionLimit);
+
+        // Creates a solver and solves the model.
+        CpSolverStatus status = solver.solve(model, cb);
+        System.out.println("Status: " + status);
+        System.out.println(cb.getSolutionCount() + " solutions found.");
+
         // Statistics.
         System.out.println("Statistics");
         System.out.printf("  conflicts: %d%n", solver.numConflicts());
